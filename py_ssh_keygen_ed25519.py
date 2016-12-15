35eaa130ed55bc26b74307d9704927e154a93e73
Peter Szabo <pts@fazekas.hu>
2016-12-15 19:37:32 +0100

    updated py_ssh_keygen_ed22159.py

--- py_ssh_keygen_ed25519.py
+++ py_ssh_keygen_ed25519.py
@@ -5,10 +5,171 @@
 #
 
 import base64
-import struct
+import hashlib
 import re
+import struct
 import sys
 
+# --- ed25519 implementation
+#
+# Based on: https://github.com/pyca/ed25519/blob/master/ed25519.py
+#
+
+# !! Namespace this.
+q = 2 ** 255 - 19
+l = 2 ** 252 + 27742317777372353535851937790883648493
+
+
+def H(m):
+    return hashlib.sha512(m).digest()
+
+
+def pow2(x, p):
+    """== pow(x, 2**p, q)"""
+    while p > 0:
+        x = x * x % q
+        p -= 1
+    return x
+
+
+def inv(z):
+    """$= z^{-1} \mod q$, for z != 0"""
+    # Adapted from curve25519_athlon.c in djb's Curve25519.
+    z2 = z * z % q                                # 2
+    z9 = pow2(z2, 2) * z % q                      # 9
+    z11 = z9 * z2 % q                             # 11
+    z2_5_0 = (z11 * z11) % q * z9 % q             # 31 == 2^5 - 2^0
+    z2_10_0 = pow2(z2_5_0, 5) * z2_5_0 % q        # 2^10 - 2^0
+    z2_20_0 = pow2(z2_10_0, 10) * z2_10_0 % q     # ...
+    z2_40_0 = pow2(z2_20_0, 20) * z2_20_0 % q
+    z2_50_0 = pow2(z2_40_0, 10) * z2_10_0 % q
+    z2_100_0 = pow2(z2_50_0, 50) * z2_50_0 % q
+    z2_200_0 = pow2(z2_100_0, 100) * z2_100_0 % q
+    z2_250_0 = pow2(z2_200_0, 50) * z2_50_0 % q   # 2^250 - 2^0
+    return pow2(z2_250_0, 5) * z11 % q            # 2^255 - 2^5 + 11 = q - 2
+
+
+d = -121665 * inv(121666) % q
+I = pow(2, (q - 1) // 4, q)
+
+
+def xrecover(y):
+    xx = (y * y - 1) * inv(d * y * y + 1)
+    x = pow(xx, (q + 3) // 8, q)
+
+    if (x * x - xx) % q != 0:
+        x = (x * I) % q
+
+    if x % 2 != 0:
+        x = q-x
+
+    return x
+
+
+By = 4 * inv(5)
+Bx = xrecover(By)
+B = (Bx % q, By % q, 1, (Bx * By) % q)
+ident = (0, 1, 1, 0)
+
+
+def edwards_add(P, Q):
+    # This is formula sequence 'addition-add-2008-hwcd-3' from
+    # http://www.hyperelliptic.org/EFD/g1p/auto-twisted-extended-1.html
+    (x1, y1, z1, t1) = P
+    (x2, y2, z2, t2) = Q
+
+    a = (y1-x1)*(y2-x2) % q
+    b = (y1+x1)*(y2+x2) % q
+    c = t1*2*d*t2 % q
+    dd = z1*2*z2 % q
+    e = b - a
+    f = dd - c
+    g = dd + c
+    h = b + a
+    x3 = e*f
+    y3 = g*h
+    t3 = e*h
+    z3 = f*g
+
+    return (x3 % q, y3 % q, z3 % q, t3 % q)
+
+
+def edwards_double(P):
+    # This is formula sequence 'dbl-2008-hwcd' from
+    # http://www.hyperelliptic.org/EFD/g1p/auto-twisted-extended-1.html
+    (x1, y1, z1, t1) = P
+
+    a = x1*x1 % q
+    b = y1*y1 % q
+    c = 2*z1*z1 % q
+    # dd = -a
+    e = ((x1+y1)*(x1+y1) - a - b) % q
+    g = -a + b  # dd + b
+    f = g - c
+    h = -a - b  # dd - b
+    x3 = e*f
+    y3 = g*h
+    t3 = e*h
+    z3 = f*g
+
+    return (x3 % q, y3 % q, z3 % q, t3 % q)
+
+
+# Bpow[i] == scalarmult(B, 2**i)
+Bpow = []
+
+
+def make_Bpow():
+    P = B
+    for i in xrange(253):
+        Bpow.append(P)
+        P = edwards_double(P)
+make_Bpow()
+
+
+def scalarmult_B(e):
+    """
+    Implements scalarmult(B, e) more efficiently.
+    """
+    # scalarmult(B, l) is the identity
+    e = e % l
+    P = ident
+    for i in xrange(253):
+        if e & 1:
+            P = edwards_add(P, Bpow[i])
+        e = e // 2
+    assert e == 0, e
+    return P
+
+
+def encodepoint(P):
+    (x, y, z, t) = P
+    zi = inv(z)
+    x = (x * zi) % q
+    y = (y * zi) % q
+    bits = [(y >> i) & 1 for i in xrange(255)] + [x & 1]
+    return ''.join([
+        chr(sum([bits[i * 8 + j] << j for j in xrange(8)]))
+        for i in xrange(32)])
+
+
+def bit(h, i):
+    return (ord(h[i // 8]) >> (i % 8)) & 1
+
+
+def publickey_unsafe(sk):
+    """
+    Not safe to use with secret keys or secret data.
+
+    See module docstring.  This function should be used for testing only.
+    """
+    h = H(sk)
+    a = 2 ** 254 + sum(2 ** i * bit(h, i) for i in xrange(3, 254))
+    A = scalarmult_B(a)
+    return encodepoint(A)
+
+
+# ---
 
 def parse_fixed(data, i, size):
   if len(data) < i + size:
@@ -88,13 +249,16 @@ def parse_openssh_private_key_ed22519(data):
     raise ValueError('Expected private key size: 32')
   if private_key[32:] != public_key:
     raise ValueError('Private key does not match public key.')
+  private_key = private_key[:32]
   j, comment = parse_lenu32str(private_key_desc, j)
   if j + 8 <= len(private_key_desc):
     raise ValueError('Private key padding too long.')
   if private_key_desc[j:] != ''.join(
       chr(k) for k in xrange(1, 1 + len(private_key_desc) - j)):
     raise ValueError('Unexpected padding value.')
-  return public_key, comment, private_key[:32], checkstr1
+  if public_key != publickey_unsafe(private_key):
+    raise ValueError('Public key and private key do not match.')
+  return public_key, comment, private_key, checkstr1
 
 
 def build_openssh_private_key_ed22519(public_key, comment, private_key,
@@ -102,16 +266,24 @@ def build_openssh_private_key_ed22519(public_key, comment, private_key,
   for value in (public_key, comment, private_key, checkstr):
     if not isinstance(value, str):
       raise TypeError
-  if len(public_key) != 32:
-    raise ValueError('Expected public key size: 32')
   if len(private_key) == 64:
     if private_key[32:] != public_key:
       raise ValueError('Private key does not match public key.')
+    private_key = private_key[:32]
   elif len(private_key) != 32:
     raise ValueError('Expected private key size: 32 or 64')
+  if not public_key:
+    public_key = publickey_unsafe(private_key)
+    assert len(public_key) == 32
+  else:
+    if len(public_key) != 32:
+      raise ValueError('Expected public key size: 32')
+    if public_key != publickey_unsafe(private_key):
+      raise ValueError('Public key and private key do not match.')
   if len(checkstr) != 4:
     raise ValueError('Expected checkstr size: 4')
   # TODO(pts): Check '\r' not in comment and '\n' not in comment (for *.pub).
+
 
   data = base64.b64encode(''.join((  # No newlines.
       'openssh-key-v1\0\0\0\0\4none\0\0\0\4none\0\0\0\0\0\0\0\1\0\0\0\x33'
